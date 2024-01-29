0fa1ac0e16d7aa32c2d735923ba1890a5c3886e6
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-01-29 20:39:49 +0200

    Add Redux slice for subreddits

---
+++ src/SideBar/SubredditList/subredditListSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { mockSubreddits } from "../../RedditApi/MockSubreddits";
+
+const initialState = mockSubreddits.data.children.map((child) => {
+    return {
+        title: child.data.title,
+        url: child.data.url,
+        communityIcon: child.data.community_icon,
+    }
+});
+
+export const subredditListSlice = createSlice({
+    name: "subreddits",
+    initialState: initialState,
+    reducers: {
+        dummyReducer: (state) => state,
+    }
+});
+
+export const selectSubreddits = (state) = state.articles;
+export const { dummyReducer } = subredditListSlice.actions;
+export default subredditListSlice.reducer;
