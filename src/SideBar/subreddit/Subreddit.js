6c143271742ca79349f2b4cac8162c8deaf2dc21
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-01-31 18:51:37 +0200

    Add default icon to subreddit if no icon found on response

--- src/SideBar/subreddit/Subreddit.js
+++ src/SideBar/subreddit/Subreddit.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { miniredditIcon } from "../../Utils/Icons/miniRedditIcon";
 
 function Subreddit( {subreddit} ) {
     const icon = subreddit.iconImg ? subreddit.iconImg : subreddit.communityIcon
     return (
         <div>
-            <img src={icon} alt={subreddit.displayNamePrefixed + " icon"}></img>
+            {icon ? <img src={icon} alt={subreddit.displayNamePrefixed + " icon"}></img> : miniredditIcon }
             <h2>{subreddit.displayName}</h2>
             <p></p>
         </div>
