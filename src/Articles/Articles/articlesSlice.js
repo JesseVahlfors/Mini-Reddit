dfc5b68ff78511f5f46969622c99b4a3ea720edf
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-02-13 20:17:08 +0200

    Create mock and api versions of articleSlice and make first fetch

--- src/Articles/Articles/articlesSlice.js
+++ src/Articles/Articles/articlesSlice.js
@@ -1,35 +1,55 @@
-import { mockThread } from "../../RedditApi/MockApi";
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = mockThread.data.children.map((child) => {
-    const image =
-        child.data.preview &&
-        child.data.preview.images &&
-        child.data.preview.images.length > 0
-            ? child.data.preview.images[0].source.url.replace(/&amp;/g, '&')
-            : null;
+const initialState = [];
 
-    return {
-        title: child.data.title,
-        author: child.data.author,
-        paragraph: child.data.selftext,
-        image: image,
-        subreddit: child.data.subreddit_name_prefixed,
-        id: child.data.id,
-        time: child.data.created_utc,
-        score: child.data.score,
-        media: child.data.media,
-        media_metadata: child.data.media_metadata,
-    };
+export const fetchArticles = createAsyncThunk("articles/fetchArticles", async () => {
+    try {
+        const response = await axios.get("https://www.reddit.com/r/Home.json?raw_json=1");
+        return response.data.data.children.map((child) => {
+            const image =
+            child.data.preview &&
+            child.data.preview.images &&
+            child.data.preview.images.length > 0
+                ? child.data.preview.images[0].source.url.replace(/&amp;/g, '&')
+                : null;
+            
+            return {
+                title: child.data.title,
+                author: child.data.author,
+                paragraph: child.data.selftext,
+                image: image,
+                subreddit: child.data.subreddit_name_prefixed,
+                id: child.data.id,
+                time: child.data.created_utc,
+                score: child.data.score,
+                media: child.data.media,
+                media_metadata: child.data.media_metadata,
+            };
+        });
+    } catch (error) {
+        console.error("Error fetching articles:", error);
+        throw error;
+    }
 });
 
 export const articlesSlice = createSlice({
     name: 'articles',
-    initialState: initialState,
-    reducers: {
-        dummyReducer: (state) => state,
-    }
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchArticles.fulfilled, (state, action) => {
+                return action.payload;
+            })
+            .addCase(fetchArticles.pending, (state) => {
+                //handle loading
+            })
+            .addCase(fetchArticles.rejected, (state, action) => {
+                //handle rejected
+            });
+    },
 });
+
 export const selectArticles = (state) => state.articles
-export const { dummyReducer } = articlesSlice.actions;
 export default articlesSlice.reducer;
---
+++ src/Articles/Articles/articlesSliceMock.js
@@ -0,0 +1,35 @@
+import { mockThread } from "../../RedditApi/MockApi";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = mockThread.data.children.map((child) => {
+    const image =
+        child.data.preview &&
+        child.data.preview.images &&
+        child.data.preview.images.length > 0
+            ? child.data.preview.images[0].source.url.replace(/&amp;/g, '&')
+            : null;
+
+    return {
+        title: child.data.title,
+        author: child.data.author,
+        paragraph: child.data.selftext,
+        image: image,
+        subreddit: child.data.subreddit_name_prefixed,
+        id: child.data.id,
+        time: child.data.created_utc,
+        score: child.data.score,
+        media: child.data.media,
+        media_metadata: child.data.media_metadata,
+    };
+});
+
+export const articlesSliceMock = createSlice({
+    name: 'articles',
+    initialState: initialState,
+    reducers: {
+        dummyReducer: (state) => state,
+    }
+});
+export const selectArticles = (state) => state.articles
+export const { dummyReducer } = articlesSliceMock.actions;
+export default articlesSliceMock.reducer;
