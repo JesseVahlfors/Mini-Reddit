461d86c4b7bfef8d94d7f281e199ded7d37881ec
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-02-14 17:29:07 +0200

    Modify SubredditSelector to work with api

--- src/SubredditsSelector/SubredditSelector/SubredditSelector.js
+++ src/SubredditsSelector/SubredditSelector/SubredditSelector.js
@@ -1,15 +1,20 @@
 import React, { useState} from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSubreddits } from "../../Sidebar/SubredditList/subredditListSlice";
+import { fetchArticles } from "../../Articles/Articles/articlesSlice";
 
 function SubredditSelector() {
     const subreddits = useSelector(selectSubreddits);
+    const dispatch = useDispatch();
     const [selectedSubreddit, setSelectedSubreddit] = useState("");
 
     const handleSubredditChange = (event) => {
         setSelectedSubreddit(event.target.value);
+        if (selectSubreddits !== event.target.value){
+        dispatch(fetchArticles(selectedSubreddit))
+        }
     }
-
+    
     return (
         <div className="subreddit-selector">
             <label htmlFor="subreddit">Select Subreddit:</label>
@@ -18,7 +23,6 @@ function SubredditSelector() {
             value={selectedSubreddit}
             onChange={handleSubredditChange}
             >
-                <option value="">-- All Subreddits --</option>
                 {subreddits.map((subreddit) => (
                     <option key={subreddit.id} value={subreddit.displayName}>
                         {subreddit.displayName}
