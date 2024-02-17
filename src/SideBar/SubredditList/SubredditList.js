45c78461668e8818650d14c2cc45f8761218ed3b
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-02-17 15:16:00 +0200

    fixed path issue

--- src/SideBar/SubredditList/SubredditList.js
+++ src/SideBar/SubredditList/SubredditList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchSubreddits, selectSubreddits } from "./subredditListSlice";
+import { fetchSubreddits, selectSubreddits } from "../../Sidebar/SubredditList/subredditListSlice";
 import Subreddit from "../../Sidebar/Subreddit/Subreddit";
 
 function SubredditsList() {
