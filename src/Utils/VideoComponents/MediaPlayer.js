6a71a2ef4dd15a65a12a541c12d987d49f34493c
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-02-21 20:48:47 +0200

    Add url and aspectRatio setup into a useMemo

--- src/Utils/VideoComponents/MediaPlayer.js
+++ src/Utils/VideoComponents/MediaPlayer.js
@@ -1,31 +1,78 @@
 import videojs from "video.js";
 import 'video.js/dist/video-js.css';
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setActivePlayer } from "./mediaPlayerSlice";
 
 
 function MediaPlayer({ media, playerId }) {
-    const {
-        fallback_url,
-        width: videoWidth,
-        height: videoHeight,
-        hasAudio,
-    } = media.reddit_video
-
+    
+    const {has_audio} = media.reddit_video;
     const dispatch = useDispatch();
     const activePlayer = useSelector((state) => state.mediaPlayer.activePlayer);
-
-    const videoUrl = fallback_url.split("?")[0];
-    const audioUrl = hasAudio ? videoUrl.split("_")[0] + "_AUDIO_128.mp4": null;
+    
     
     const videoRef = useRef(null);
     const playerRef = useRef(null);
-    const audioRef = useRef(hasAudio ? new Audio(audioUrl) : null);
+    const audioRef = useRef(null);
 
-    const calculateAspectRatio = (width, height) => `${width}:${height}`;
-    const aspectRatio = calculateAspectRatio(videoWidth, videoHeight);
-
+    const { videoUrl, audioUrl, aspectRatio} = useMemo(()=> {
+        
+        const { fallback_url, width, height} = media.reddit_video;
+        const videoUrl = fallback_url.split("?")[0];
+        const audioUrl = has_audio ? videoUrl.split("_")[0] + "_AUDIO_128.mp4": null;
+        const aspectRatio = `${width}:${height}`;
+        
+        return { videoUrl, audioUrl, aspectRatio}
+    }, [media])
+   
+    
+    useEffect(() => {
+        console.log(has_audio)
+        audioRef.current = has_audio ? new Audio(audioUrl) : null
+        
+        if (!playerRef.current) {
+            playerRef.current = videojs(videoRef.current, {
+                controls: true,
+                fluid: true,
+                aspectRatio: aspectRatio,
+                sources: [{
+                    src: videoUrl,
+                    type: 'video/mp4',
+                }],
+                height: media.reddit_video.height,
+                width: media.reddit_video.width,
+                
+            });
+            
+            if (has_audio) { 
+                playerRef.current.on('play', () => audioRef.current.play());
+                playerRef.current.on('pause', () => audioRef.current.pause());
+                playerRef.current.on('timeupdate', () => {
+                    const difference = Math.abs(playerRef.current.currentTime() - audioRef.current.currentTime);
+                    if (difference > 0.5) {
+                        audioRef.current.currentTime = playerRef.current.currentTime();
+                    }
+                });
+            }
+        }
+        // TODO: Audio Volume
+        
+        return () => {
+            if (playerRef.current) {
+                playerRef.current.dispose();
+                playerRef.current = null;
+            }
+            
+            if (audioRef.current) {
+                audioRef.current.pause();
+                audioRef.current.src = "";
+                audioRef.current.load();
+                audioRef.current = null;
+            }
+        };
+    }, [media]); 
+    
     const handlePlay = () => {
        dispatch(setActivePlayer(playerId))
     }
@@ -37,50 +84,6 @@ function MediaPlayer({ media, playerId }) {
         }
     }, [activePlayer, playerId])
     
-    useEffect(() => {
-        if (!playerRef.current) {
-            playerRef.current = videojs(videoRef.current, {
-                controls: true,
-                fluid: true,
-                aspectRatio: aspectRatio,
-                sources: [{
-                    src: videoUrl,
-                    type: 'video/mp4',
-                }],
-                videoHeight,
-                videoWidth
-            
-            });
-
-            if (hasAudio) { 
-                playerRef.current.on('play', () => audioRef.current.play());
-                playerRef.current.on('pause', () => audioRef.current.pause());
-                playerRef.current.on('timeupdate', () => {
-                    const difference = Math.abs(playerRef.current.currentTime() - audioRef.current.currentTime);
-                    if (difference > 0.5) {
-                        audioRef.current.currentTime = playerRef.current.currentTime();
-                    }
-                });
-            }
-        }
-        // TODO: Audio stop and Volume
-        
-        return () => {
-            if (playerRef.current) {
-                playerRef.current.dispose();
-                playerRef.current = null;
-            }
-
-            if (audioRef.current) {
-                audioRef.current.pause();
-                audioRef.current.src = "";
-                audioRef.current.load();
-                audioRef.current = null;
-            }
-        };
-    }, [media]); 
-
-
     return(
         <div data-vjs-player onClick={handlePlay}>
             <video ref={videoRef} className="video-js"></video>
