ad2c75e5a16fd7e8a3b99d0e70eb9054efaf0be5
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-03-27 23:06:17 +0200

    feat: Integrate and test new components in DetailedArticle
    Integrated `MediaComponent`, `TimeAgo`, and `useImageOverlay` hook into the `DetailedArticle` component to enhance media handling, display relative timestamps, and manage image overlays. Additionally, utilized the `useRedditScore` hook for dynamic score management. Conducted initial testing to ensure functionality aligns with expected behavior, improving the overall user experience and article detail presentation. Modifications include upvote/downvote functionality, dynamic media rendering based on article content, and overlay support for images.
    
    This update significantly elevates the interactivity and visual appeal of article details.

--- src/Articles/DetailedArticle/DetailedArticle.js
+++ src/Articles/DetailedArticle/DetailedArticle.js
@@ -1,118 +1,43 @@
 import React, {useEffect, useState} from "react";
 import Comments from "../Comments/Comments";
 import ReactMarkdown from 'react-markdown'
-import { formatDate } from "../../Utils/Funcs/timeFormat";
-import { getTimeDifferenceString } from "../../Utils/Funcs/time";
-import ImageGallery from "../../Utils/ImageComponents/ImageGallery";
-import MediaPlayer from "../../Utils/VideoComponents/MediaPlayer";
-import EmbedVideoComponent from "../../Utils/VideoComponents/EmbedVideoComponent";
-import TwitchEmbedComponent from "../../Utils/VideoComponents/TwitchEmbedComponent";
-import TwitterEmbedComponent from "../../Utils/VideoComponents/TwitterEmbedComponent";
 import ImageOverlay from "../../Utils/ImageComponents/ImageOverlay";
 import { useDispatch } from "react-redux";
 import { fetchComments } from "../Comments/commentsSlice";
 import './DetailedArticle.css'
+import { useImageOverlay } from "../../Utils/Hooks/useImageOverlay";
+import MediaComponent from "../../Utils/Components/MediaComponent";
+import TimeAgo from "../../Utils/Components/TimeAgo";
+import { useRedditScore } from "../../Utils/Hooks/useRedditScore";
 
 
 function DetailedArticle( { article, onBackButtonClick } ) { 
     const dispatch = useDispatch();
 
-    //Time utilities
-    const navigatorDateFormat = formatDate(article.time)
-    const date = new Date(article.time * 1000);
-    const formattedISODate = date.toISOString();
 
     //Reddit Score
-    const [articleScore, setArticleScore] = useState(article.score)
-    const [isIncremented, setIsIncremented] = useState(false)
-    const [isDecremented, setIsDecremented] = useState(false)
-    
+    const { score,
+        incrementScore,
+        decrementScore,
+        isIncremented,
+        isDecremented
+       } = useRedditScore(article.score);
 
     const handleUpArrowClick = (event) => {
         event.stopPropagation();
-        if(isIncremented) {
-        setArticleScore(articleScore -1);
-       } else {
-        if (isDecremented) {
-            setArticleScore(articleScore +2);
-            setIsDecremented(false);
-        } else {
-            setArticleScore(articleScore +1);
-        }
-       }
-       setIsIncremented(!isIncremented);
+        incrementScore()
     }
 
     const handleDownArrowClick = (event) => {
         event.stopPropagation();
-        if(isDecremented) {
-            setArticleScore(articleScore +1);
-       } else {
-            if (isIncremented) {
-                setArticleScore(articleScore -2);
-
-                setIsIncremented(false);
-            } else {
-                setArticleScore(articleScore -1);
-            }
-       }
-       setIsDecremented(!isDecremented);
+        decrementScore();
     }
 
     const upArrowClass = `up arrow ${isIncremented ? 'green-arrow' : ''}`;
     const downArrowClass = `down arrow ${isDecremented ? 'green-arrow' : ''}`
 
     //Image overlay
-    const [isOverlayOpen, setIsOverlayOpen] = useState(false);
-    const [overlayContent, setOverlayContent] = useState(null);
-
-    const handleOpenOverlay = (content) => {
-        setOverlayContent(content);
-        setIsOverlayOpen(true);
-    }
-
-    const handleCloseOverlay = () => {
-        setIsOverlayOpen(false);
-    }
-    
-    //Article media
-
-    function createImageElement(resolutions, title) {
-        if (resolutions.length === 0) {
-            return null
-        }
-
-        const srcSet = resolutions.map(resolution => `${resolution.url} ${resolution.width}w`).join(', ');
-
-        const largestImage= resolutions.reduce((prev, current) => (prev.width > current.width) ? prev : current);
-
-        return (
-            <img src={largestImage.url} srcSet={srcSet} alt={title} sizes="(min-width: 1415px) 750px, (min-width: 768px) 50vw, 100vw" />
-        );
-    }
-
-    //Media selection
-    let mediaToRender = null;
-    if (article.media?.oembed?.type === 'video') {
-       mediaToRender = <EmbedVideoComponent html={article.media.oembed.html}/>;
-    } else if (article.media?.type === 'twitter.com') {
-        mediaToRender = <TwitterEmbedComponent html={article.media.oembed.html}/>;
-    } else if (article.media?.type === 'clips.twitch.tv' || article.media?.type === 'twitch.tv') {
-        mediaToRender = <TwitchEmbedComponent html={article.media.oembed.html}/>;
-    } else if (article.media?.reddit_video) {
-        mediaToRender = <MediaPlayer media={article.media} playerId={article.id}  />;
-    } else if (article.is_gallery){
-        mediaToRender = <ImageGallery metadata={article.media_metadata} />
-    } else if (article.image.source && article.image.resolutions){
-        const imageElement = createImageElement(article.image.resolutions, article.title)
-        mediaToRender = (
-            <button onClick={() => handleOpenOverlay(imageElement)}>
-                {imageElement}
-            </button> 
-        )
-    } else if (article.url.includes("https://kick.com")){
-        mediaToRender = <a href={article.url} title={article.title} >{article.url}</a>
-    }
+    const { isOverlayOpen, overlayContent, handleCloseOverlay } = useImageOverlay();
 
     //Comment handling
     const [commentClicked, setCommentClicked] = useState(false);
@@ -145,7 +70,7 @@ function DetailedArticle( { article, onBackButtonClick } ) {
                 <div className="article-metadata">
                     <div className="article-subreddit-container">
                         <h4 className="article-subreddit">{article.subreddit}</h4>
-                        <time dateTime={formattedISODate} title={navigatorDateFormat}>{getTimeDifferenceString(article.time)}</time>
+                        <TimeAgo timestamp={article.time}/>
                     </div>
                     <p className="article-topbar-paragraph">{article.author}</p>
                 </div>
@@ -153,13 +78,13 @@ function DetailedArticle( { article, onBackButtonClick } ) {
             <div className="article-wrapper">
                 <div className="score">
                     <button className={upArrowClass} onClick={handleUpArrowClick}></button>
-                    <p>{articleScore}</p>
+                    <p>{score}</p>
                     <button className={downArrowClass} onClick={handleDownArrowClick}></button>
                 </div>
                 <div className="article-content">
                     <h2>{article.title}</h2>
                     <div className="media-wrapper">
-                        {mediaToRender}
+                        <MediaComponent article={article}/>
                         <ImageOverlay isOpen={isOverlayOpen} onClose={handleCloseOverlay}>
                             {overlayContent}
                         </ImageOverlay>
