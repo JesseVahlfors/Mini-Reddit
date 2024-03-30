108ff381e2e38b719ff02b2c3afc053efcafc5ea
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-03-30 08:03:18 +0200

    feat: Enhance image loading performance with lazy attribute
    Added the `loading="lazy"` attribute to images created by the CreateImageElement function. This change leverages browser-level lazy loading, improving page load performance by deferring the loading of off-screen images until they are needed. This enhancement is particularly beneficial for pages with numerous images, reducing initial load time and conserving bandwidth.

--- src/Utils/ImageComponents/CreateImageElement.js
+++ src/Utils/ImageComponents/CreateImageElement.js
@@ -10,7 +10,7 @@ function CreateImageElement(resolutions, title) {
     const largestImage= resolutions.reduce((prev, current) => (prev.width > current.width) ? prev : current);
 
     return (
-        <img src={largestImage.url} srcSet={srcSet} alt={title} sizes="(min-width: 1415px) 750px, (min-width: 768px) 50vw, 100vw" />
+        <img src={largestImage.url} srcSet={srcSet} alt={title} sizes="(min-width: 1415px) 750px, (min-width: 768px) 50vw, 100vw" loading="lazy" />
     );
 }
 
