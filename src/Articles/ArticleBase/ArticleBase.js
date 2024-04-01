8eba5e78bdd9f0cb15dd9ecf241aca631c2a518d
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-04-01 08:13:07 +0300

    feat: Implement ArticleBase for unified article structure
    Introduced ArticleBase component as a foundational element to unify the structure of articles across the application. This component integrates MetadataComponent for article metadata, ScoreComponent for scoring functionality, and MediaComponent for media handling, alongside ImageOverlay for image viewing. It supports both detailed and summary views through conditional rendering and is designed to enhance code reuse and maintain consistency in article presentation.

--- src/Articles/ArticleBase/ArticleBase.js
+++ src/Articles/ArticleBase/ArticleBase.js
@@ -1,34 +1,37 @@
 import React from "react";
-import { useRedditScore } from "../../Utils/Hooks/useRedditScore";
+import { useImageOverlay } from "../../Utils/Hooks/useImageOverlay";
+import MediaComponent from "../../Utils/Components/MediaComponent";
+import ImageOverlay from "../../Utils/ImageComponents/ImageOverlay";
+import ScoreComponent from "../../Utils/Components/ScoreComponent";
+import MetadataComponent from "../../Utils/Components/MetadataComponent";
 
-function ArticleBase({ article, onClick, isDetailed = false }) {
-    const {
-        score,
-        incrementScore,
-        decrementScore,
-        isIncremented,
-        isDecremented
-    } = useRedditScore(article.score)
 
-    const handleDownArrowClick = (event) => {
+function ArticleBase({ article, onClick, isDetailed = false, children }) {
+    //Image overlay
+    const { isOverlayOpen, overlayContent, handleCloseOverlay } = useImageOverlay();
+     //Prevent selecting article with image
+     const handleImageClick = (event) => {
         event.stopPropagation();
-        decrementScore(article.score)
     }
-
-    const handleUpArrowClick = (event) => {
-        event.stopPropagation();
-        incrementScore(article.score)
-    }
-
+    
     return (
         <div className="article card" id={`article-${article.id}`} onClick={!isDetailed? onClick : undefined}>
-            
-            {!isDetailed && (
-
-            )}
-            {isDetailed && (
-
-            )}
+            <MetadataComponent article={article} /> 
+            <div className="article-wrapper">
+                <ScoreComponent article={article} />
+                <div className="article-content" onClick={onClick}>
+                    <h2>{article.title}</h2>
+                    <div className="media-wrapper" onClick={handleImageClick}>
+                        <MediaComponent article={article}/>
+                        <ImageOverlay isOpen={isOverlayOpen} onClose={handleCloseOverlay}>
+                            {overlayContent}
+                        </ImageOverlay>
+                    </div>
+                    {children}
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default ArticleBase;
