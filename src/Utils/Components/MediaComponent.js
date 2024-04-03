3b9d8b4fb8f0d91a252d162d967a77a2c49809e3
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-04-03 08:40:52 +0300

    feat(MediaComponent): Add YouTube Shorts rendering & icons container for Shorts and Kick links
    Enhanced MediaComponent to support rendering of YouTube Shorts, including a new method that dynamically renders YouTube Shorts media content. Also introduced a unified container with icons for both YouTube Shorts and Kick links, streamlining the visual representation of these media types within articles. The update includes integration of custom SVG icons for YouTube Shorts and Kick, improving the UI/UX for media interaction.

--- src/Utils/Components/MediaComponent.js
+++ src/Utils/Components/MediaComponent.js
@@ -16,6 +16,8 @@ import MediaPlayer from "../VideoComponents/MediaPlayer";
 import ImageGallery from "../ImageComponents/ImageGallery";
 import { useImageOverlay } from "../Hooks/useImageOverlay";
 import CreateImageData from "../ImageComponents/CreateImageData";
+import Kick_logo from "../../Utils/Icons/Kick_logo.svg"
+import Youtube_shorts_icon from "../../Utils/Icons/Youtube_shorts_icon.svg"
 
 function MediaComponent({ article }) {
     const { handleOpenOverlay } = useImageOverlay();
@@ -41,7 +43,41 @@ function MediaComponent({ article }) {
                 </button>
             );
         },
-        kick: () => <a href={article.url} title={article.title}>{article.url}</a>,
+        kick: () => {
+        return (
+        <a href={article.url} title={article.title} className="kick-link">
+            <img src={Kick_logo} alt='Kick.com logo'/>
+            <button className="kick-button">
+                Open
+            </button>
+        </a>
+        )},
+        youtubeShorts: () => {
+            const imageData = CreateImageData(article.image.resolutions, article.title);
+            return (
+                <>
+                    <img
+                        src={imageData.url}
+                        srcSet={imageData.srcSet}
+                        alt={imageData.alt}
+                        sizes={imageData.sizes}
+                        loading={imageData.loading}
+                    />
+                    <a href={article.url} title={article.title} className="kick-link">
+                        <p className="shorts">
+                            <img 
+                            src={Youtube_shorts_icon} 
+                            alt='Youtube shorts logo'
+                            className="youtube-icon"
+                            />
+                            Shorts
+                        </p>
+                        <button className="kick-button">
+                            Open
+                        </button>
+                    </a>
+                </>
+        )}
     }
 
     // Determines the media type of the article for selecting the appropriate renderer.
@@ -51,6 +87,7 @@ function MediaComponent({ article }) {
         if (article.media?.type === 'clips.twitch.tv' || article.media?.type === 'twitch.tv') return 'twitch'; 
         if (article.media?.reddit_video) return 'reddit_video';
         if (article.is_gallery) return 'gallery';
+        if (article.url.includes("https://youtube.com/shorts")) return 'youtubeShorts'
         if (article.image.source && article.image.resolutions) return 'image';
         if (article.url.includes("https://kick.com")) return 'kick';
         return null; //default if no media type matches
